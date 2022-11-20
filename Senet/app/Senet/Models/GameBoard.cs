e7405dc3b8653a977e3cb61d6f999b9fa1fad2e2
Brent Acs <brent.mann@gmail.com>
2022-11-20 18:31:50 -0500

    WIP

---
+++ Senet/app/Senet/Models/GameBoard.cs
@@ -0,0 +1,22 @@
+﻿namespace Senet.Models;
+
+public class GameBoard : BindableObject
+{
+   private const int TileCount = 30;
+
+   private GameBoardTile[] _tiles = new GameBoardTile[ TileCount ];
+
+   public GameBoardTile this[int index]
+   {
+      get => GetTile(index);
+      set => SetTile(index, value);
+   }
+
+   public GameBoardTile GetTile(int index) => _tiles[ index ];
+   public void SetTile(int index, GameBoardTile tile) => _tiles[ index ] = tile;
+}
+
+public class GameBoardTile : BindableObject
+{
+   public int Index { get; private set; }
+}
